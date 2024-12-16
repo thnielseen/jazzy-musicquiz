928a0160ce22198b4593396b5ccc7c4e7b016e6c
Egil Eskilsson <egil@egileskilsson.se>
2024-12-16 15:18:01 +0100

    Update vite.config.ts for sass variables

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   'base': '/fed24d-js-intro-inl-2-quiz-jazzy/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/scss/abstracts/variables/_mx-*";`
+      }
+    }
+  }
 });
