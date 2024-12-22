f0bd164c61177149359a756bc5f8eacf3d9a655b
angien90 <angelicanylander@hotmail.com>
2024-12-22 15:34:24 +0100

    Add connection to counter function

--- src/ts/startButton.ts
+++ src/ts/startButton.ts
@@ -1,4 +1,5 @@
-import { Timer } from './timerFunction';  // Importera Timer-klassen
+import { Timer } from './timerFunction';  // Importera timer-funktionen
+import { getNextQuestion } from './questionCounter'; // Importerar räknaren för att stoppa spelomgången
 
 export function setupStartButton(startButtonId: string, timeDisplayId: string) {
     const startButton = document.getElementById(startButtonId)!;
@@ -12,7 +13,8 @@ export function setupStartButton(startButtonId: string, timeDisplayId: string) {
     // Eventlyssnare på startknappen som startar spelet och triggar timern, ..............
     startButton.addEventListener('click', () => {
         const myTimer = new Timer(timeDisplay);  // Skapa Timer-objektet
-        console.log('Spelet startas!');
         myTimer.start();  // Starta timern
+        getNextQuestion(); // Startar counter funktion när spelet startar. 
+        console.log('Spelet startas!');
     });
 }
