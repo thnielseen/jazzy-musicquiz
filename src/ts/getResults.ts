015e3a0add9e84f515f163f6cb06f61d3c5f9d25
angien90 <angelicanylander@hotmail.com>
2024-12-25 16:37:23 +0100

    Changed place for timerDisplay

--- src/ts/getResults.ts
+++ src/ts/getResults.ts
@@ -1,7 +1,7 @@
 import { Timer } from './timerFunction';
 import { calculateScore, checkUserAnswer } from './getQuizQuestions';  // Exempel på hur du importerar logik för att räkna poäng och kontrollera svar.
 
-export function collectGameData(quizQuestions: QuizQuestion[]): { correctAnswers: number, totalScore: number, totalTime: number } {
+export function collectGameData(quizQuestions: getQuizQuestion[]): { correctAnswers: number, totalScore: number, totalTime: number } {
     const correctAnswers = quizQuestions.filter(q => q.isUserAnswerCorrect).length;
     const totalScore = quizQuestions.reduce((acc, q) => acc + q.score, 0);
     const totalTime = Timer.getLastSavedTime() || 0; // Hämta senaste sparade tid
